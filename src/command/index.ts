952223e2e9288bfbd0ccb3cbdbf39bd52952192d
adnpark <aidenp.dev@gmail.com>
2023-12-31 04:09:57 +0900

    feat: add generate-salt command

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,38 +1,39 @@
 #!/usr/bin/env node
-import figlet from "figlet";
-import chalk from "chalk";
-import Table from "cli-table3";
-import { Command } from "commander";
+import figlet from 'figlet';
+import chalk from 'chalk';
+import Table from 'cli-table3';
+import crypto from 'crypto';
+import { Command } from 'commander';
 import {
   computeAddress,
   deployContracts,
   findDeployment,
   getDeployerAddress,
-} from "../action";
-import { DEPLOYER_CONTRACT_ADDRESS, getSupportedChains } from "../constant";
+} from '../action';
+import { DEPLOYER_CONTRACT_ADDRESS, getSupportedChains } from '../constant';
 import {
   clearFiles,
   ensureHex,
   processAndValidateChains,
   readBytecodeFromFile,
   validateInputs,
-} from "../utils";
+} from '../utils';
 
 export const program = new Command();
 
 program
-  .name("ZeroDev-Multichain-Deployer")
+  .name('ZeroDev-Multichain-Deployer')
   .description(
-    "A CLI tool for deploying contracts to multichain with account abstraction"
+    'A CLI tool for deploying contracts to multichain with account abstraction'
   )
-  .usage("zerodev <command> [options]")
-  .version("1.0.0");
+  .usage('zerodev <command> [options]')
+  .version('1.0.0');
 
 program.helpInformation = function () {
   const asciiArt = chalk.blueBright(
-    figlet.textSync("ZeroDev Deployer", {
-      horizontalLayout: "default",
-      verticalLayout: "default",
+    figlet.textSync('ZeroDev Deployer', {
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
       width: 100,
       whitespaceBreak: true,
     })
@@ -43,32 +44,32 @@ program.helpInformation = function () {
 };
 
 program
-  .command("chains")
-  .description("Show the list of available chains")
+  .command('chains')
+  .description('Show the list of available chains')
   .action(() => {
     const chains = getSupportedChains().map((chain) => [
       chain.name,
-      chain.type === "mainnet"
+      chain.type === 'mainnet'
         ? chalk.blue(chain.type)
         : chalk.green(chain.type),
     ]);
 
     const table = new Table({
-      head: ["Name", "Type"],
-      chars: { mid: "", "left-mid": "", "mid-mid": "", "right-mid": "" },
+      head: ['Name', 'Type'],
+      chars: { mid: '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
     });
 
     chains.forEach((chain) => table.push(chain));
 
-    console.log("[Available chains]");
+    console.log('[Available chains]');
     console.log(table.toString());
   });
 
 program
-  .command("compute-address")
-  .description("Compute the address to be deployed")
-  .argument("<path-to-bytecode>", "file path of bytecode to deploy")
-  .argument("<salt>", "salt to be used for create2")
+  .command('compute-address')
+  .description('Compute the address to be deployed')
+  .argument('<path-to-bytecode>', 'file path of bytecode to deploy')
+  .argument('<salt>', 'salt to be used for create2')
   .action((pathToBytecode: string, salt: string) => {
     const bytecode = readBytecodeFromFile(pathToBytecode);
     const address = computeAddress(
@@ -80,7 +81,7 @@ program
   });
 
 program
-  .command("get-deployer-address")
+  .command('get-deployer-address')
   .description("Get the deployer's address")
   .action(async () => {
     const address = await getDeployerAddress();
@@ -88,23 +89,23 @@ program
   });
 
 program
-  .command("deploy")
+  .command('deploy')
   .description(
-    "Deploy contracts deterministically using CREATE2, in order of the chains specified"
+    'Deploy contracts deterministically using CREATE2, in order of the chains specified'
   )
   .argument(
-    "<path-to-bytecode>",
-    "file path of bytecode to deploy, a.k.a. init code"
+    '<path-to-bytecode>',
+    'file path of bytecode to deploy, a.k.a. init code'
   )
-  .argument("<salt>", "salt to be used for CREATE2")
+  .argument('<salt>', 'salt to be used for CREATE2')
   .option(
-    "-c, --chains [CHAINS]",
-    "list of chains for deploying contracts, with all selected by default",
-    "all"
+    '-c, --chains [CHAINS]',
+    'list of chains for deploying contracts, with all selected by default',
+    'all'
   )
-  .option("-e, --expected-address [ADDRESS]", "expected address to confirm")
-  .option("-t, --testnet-all", "select all testnets", false)
-  .option("-m, --mainnet-all", "select all mainnets", false)
+  .option('-e, --expected-address [ADDRESS]', 'expected address to confirm')
+  .option('-t, --testnet-all', 'select all testnets', false)
+  .option('-m, --mainnet-all', 'select all mainnets', false)
   .action((pathToBytecode: string, salt: string, options) => {
     const { chains, expectedAddress, testnetAll, mainnetAll } = options;
     const bytecode = readBytecodeFromFile(pathToBytecode);
@@ -125,22 +126,22 @@ program
 
 /** @notice 400 error on base-sepolia */
 program
-  .command("check-deployment")
+  .command('check-deployment')
   .description(
-    "check whether the contract has already been deployed on the specified networks"
+    'check whether the contract has already been deployed on the specified networks'
   )
   .argument(
-    "<path-to-bytecode>",
-    "file path of bytecode to deploy, a.k.a. init code"
+    '<path-to-bytecode>',
+    'file path of bytecode to deploy, a.k.a. init code'
   )
-  .argument("<salt>", "salt used for depolyment")
+  .argument('<salt>', 'salt used for depolyment')
   .option(
-    "-c, --chains [CHAINS]",
-    "list of chains to check, with all selected by default",
-    "all"
+    '-c, --chains [CHAINS]',
+    'list of chains to check, with all selected by default',
+    'all'
   )
-  .option("-t, --testnet-all", "select all testnets", false)
-  .option("-m, --mainnet-all", "select all mainnets", false)
+  .option('-t, --testnet-all', 'select all testnets', false)
+  .option('-m, --mainnet-all', 'select all mainnets', false)
   .action(async (pathToBytecode: string, salt: string, options) => {
     const { chains, testnetAll, mainnetAll } = options;
     const bytecode = readBytecodeFromFile(pathToBytecode);
@@ -155,16 +156,31 @@ program
       await findDeployment(ensureHex(bytecode), ensureHex(salt), chainObjects);
 
     console.log(`contract address: ${contractAddress}`);
-    console.log("deployed on:");
+    console.log('deployed on:');
     deployedChains.forEach((chain) => console.log(`- ${chain.name}`));
-    console.log("not deployed on:");
+    console.log('not deployed on:');
     notDeployedChains.forEach((chain) => console.log(`- ${chain.name}`));
   });
 
 program
-  .command("clear-log")
-  .description("clear the log files")
+  .command('clear-log')
+  .description('clear the log files')
   .action(() => {
-    clearFiles("./log");
-    console.log("done!");
+    clearFiles('./log');
+    console.log('done!');
+  });
+
+program
+  .command('generate-salt')
+  .description('generate a random 32 bytes salt')
+  .option('-i, --input <input>', 'input to be converted to salt')
+  .action((options) => {
+    let salt;
+    if (options.input) {
+      const inputNum = BigInt(options.input);
+      salt = inputNum.toString(16).padStart(64, '0'); // pad the input with zeros to make it 32 bytes
+    } else {
+      salt = crypto.randomBytes(32).toString('hex');
+    }
+    console.log(`Generated salt: ${ensureHex(salt)}`);
   });
