627bb0993885172772c375af4e4f5fcb2e1b14fb
adnpark <aidenp.dev@gmail.com>
2023-12-30 18:06:01 +0900

    refactor: change type to enum

--- src/constant.ts
+++ src/constant.ts
@@ -26,7 +26,10 @@ export const DEPLOYER_CONTRACT_ADDRESS =
 
 export const ENTRYPOINT = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
 
-export type Network = 'mainnet' | 'testnet';
+export enum Network {
+  mainnet = 'mainnet',
+  testnet = 'testnet',
+}
 
 export interface Chain {
   name: string;
@@ -40,102 +43,102 @@ export const getSupportedChains = (): Chain[] => [
     name: 'arbitrum',
     projectId: process.env.ARBITRUM_PROJECT_ID || null,
     viemChainObject: arbitrum,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'arbitrum-sepolia',
     projectId: process.env.ARBITRUM_SEPOLIA_PROJECT_ID || null,
     viemChainObject: arbitrumSepolia,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'avalanche',
     projectId: process.env.AVALANCHE_PROJECT_ID || null,
     viemChainObject: avalanche,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'avalanche-fuji',
     projectId: process.env.AVALANCHE_FUJI_PROJECT_ID || null,
     viemChainObject: avalancheFuji,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'base',
     projectId: process.env.BASE_PROJECT_ID || null,
     viemChainObject: base,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'base-sepolia',
     projectId: process.env.BASE_SEPOLIA_PROJECT_ID || null,
     viemChainObject: baseSepolia,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'bsc',
     projectId: process.env.BSC_PROJECT_ID || null,
     viemChainObject: bsc,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
-    name: 'mainnet',
+    name: Network.mainnet,
     projectId: process.env.MAINNET_PROJECT_ID || null,
     viemChainObject: mainnet,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'sepolia',
     projectId: process.env.SEPOILA_PROJECT_ID || null,
     viemChainObject: sepolia,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'optimism',
     projectId: process.env.OPTIMISM_PROJECT_ID || null,
     viemChainObject: optimism,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'optimism-sepolia',
     projectId: process.env.OPTIMISM_SEPOLIA_PROJECT_ID || null,
     viemChainObject: optimismSepolia,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'polygon',
     projectId: process.env.POLYGON_PROJECT_ID || null,
     viemChainObject: polygon,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'polygon-mumbai',
     projectId: process.env.POLYGON_MUMBAI_PROJECT_ID || null,
     viemChainObject: polygonMumbai,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'linea',
     projectId: process.env.LINEA_PROJECT_ID || null,
     viemChainObject: linea,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'linea-testnet',
     projectId: process.env.LINEA_TESTNET_PROJECT_ID || null,
     viemChainObject: lineaTestnet,
-    type: 'testnet',
+    type: Network.testnet,
   },
   {
     name: 'opbnb',
     projectId: process.env.OPBNB_PROJECT_ID || null,
     viemChainObject: opBNB,
-    type: 'mainnet',
+    type: Network.mainnet,
   },
   {
     name: 'astar-zkatana',
     projectId: process.env.ASTAR_ZKATANA_PROJECT_ID || null,
     viemChainObject: astarZkatana,
-    type: 'testnet',
+    type: Network.testnet,
   },
 ];
