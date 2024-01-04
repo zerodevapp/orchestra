7946e863984d0114740f7151e1f82adfb4f0724c
adnpark <aidenp.dev@gmail.com>
2024-01-05 01:56:23 +0900

    chore: remove unused import

--- src/clients/kernelAccountClient.ts
+++ src/clients/kernelAccountClient.ts
@@ -1,15 +1,6 @@
-import {
-    createEcdsaKernelAccountClient
-} from "@kerneljs/presets/zerodev"
-import { signerToEcdsaValidator } from "@kerneljs/ecdsa-validator"
-import {
-    SmartAccountClient,
-    UserOperation,
-    createSmartAccountClient
-} from "permissionless"
-import { http, createPublicClient } from "viem"
+import { createEcdsaKernelAccountClient } from "@kerneljs/presets/zerodev"
+import { SmartAccountClient } from "permissionless"
 import { privateKeyToAccount } from "viem/accounts"
-import { getZeroDevBundlerRPC, getZeroDevPaymasterRPC } from "../clients"
 import { PRIVATE_KEY } from "../config"
 import { Chain } from "../constant"
 
@@ -23,10 +14,10 @@ export const createKernelAccountClient = async (
         chain: chain.viemChainObject,
         projectId: chain.projectId,
         signer,
-    
+
         // optional
         provider: "ALCHEMY", // defaults to a recommended provider
         index: BigInt(0), // defaults to 0
-        usePaymaster: true, // defaults to true
-      })
+        usePaymaster: true // defaults to true
+    })
 }
