26b9abba2c35e7bb012ec1591cce8198f2f92b76
adnpark <aidenp.dev@gmail.com>
2024-01-05 01:56:48 +0900

    chore: format

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,14 +1,20 @@
-export const getZeroDevBundlerRPC = (projectId: string, provider?: string): string => {
+export const getZeroDevBundlerRPC = (
+    projectId: string,
+    provider?: string
+): string => {
     let rpc = `https://rpc.zerodev.app/api/v2/bundler/${projectId}`
-    if(provider) {
-      rpc += `?bundlerProvider=${provider}`
+    if (provider) {
+        rpc += `?bundlerProvider=${provider}`
     }
     return rpc
 }
-export const getZeroDevPaymasterRPC = (projectId: string, provider?: string): string => {
+export const getZeroDevPaymasterRPC = (
+    projectId: string,
+    provider?: string
+): string => {
     let rpc = `https://rpc.zerodev.app/api/v2/paymaster/${projectId}`
-    if(provider) {
-      rpc += `?paymasterProvider=${provider}`
+    if (provider) {
+        rpc += `?paymasterProvider=${provider}`
     }
     return rpc
 }
